c7ea22c0859ffd88b2c7005d0ee5196c131a4b6d
Saalim <saalim520>
2023-09-28 12:19:49 +0530

    second commit added routes and some minor renames

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
 import React from 'react'
+import Signin from './Pages/Signin'
+import Signup from './Pages/Signup'
+import About from './Pages/About'
+import Profile from './Pages/Profile'
+import Home from './Pages/home'
 
 export default function App() {
   return (
-    <h1 className="text-red-900 text-3xl">App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/signin' element={<Signin/>}></Route>
+        <Route path='/signup' element={<Signup/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/profile' element={<Profile/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
