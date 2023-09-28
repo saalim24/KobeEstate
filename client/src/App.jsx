78259c4a63a03ac3cdaf038289e0ef08af131f0d
Saalim <saalim520>
2023-09-28 14:38:35 +0530

    created header , added routes and Links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Signup from './Pages/Signup'
 import About from './Pages/About'
 import Profile from './Pages/Profile'
 import Home from './Pages/home'
+import Header from './components/header'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header></Header>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/signin' element={<Signin/>}></Route>
