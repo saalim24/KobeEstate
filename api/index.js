b27d1a92dad1ac2f762bf99aa464a4e87c5353d5
Saalim <saalim520>
2023-09-29 16:20:01 +0530

    created test api route and added some more express js and created some folders to organize files

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -17,3 +18,5 @@ mongoose
 app.listen(3000, () => {
   console.log("server is running");
 });
+
+app.use("/api/user", userRouter);
