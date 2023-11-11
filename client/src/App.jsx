1376c57804586854ac01c4cd37b73dba2e95a394
Saalim <saalim520>
2023-11-11 19:46:06 +0530

    ok

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home";
-import SignIn from "./Pages/SignIn";
-import SignUp from "./Pages/SignUp";
-import About from "./Pages/About";
-import Profile from "./Pages/Profile";
+import Home from "./pagess/Home";
+import SignIn from "./pagess/SignIn";
+import SignUp from "./pagess/SignUp";
+import About from "./pagess/About";
+import Profile from "./pagess/Profile";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
-import CreateListing from "./Pages/CreateListing";
-import UpdateListing from "./Pages/UpdateListing";
-import Listing from "./Pages/Listing";
-import Search from "./Pages/Search";
+import CreateListing from "./pagess/CreateListing";
+import UpdateListing from "./pagess/UpdateListing";
+import Listing from "./pagess/Listing";
+import Search from "./pagess/Search";
 
 export default function App() {
   return (
